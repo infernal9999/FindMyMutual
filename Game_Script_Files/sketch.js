4dda336d0760878f1d706b3bbe399e20b113a4a2
Diptanu Roy <49843997+infernal9999@users.noreply.github.com>
2023-07-10 20:50:03 +0530

    Update sketch.js

--- Game_Script_Files/sketch.js
+++ Game_Script_Files/sketch.js
@@ -1,140 +1,34 @@
-var Csize;
-var cells;
-var LIMIT;
 var board;
-var Players;
-var p1;
-var p2;
-
-var cnv;
-var h1;
-var h3;
-var s;
-var grd;
-var gs;
-var button;
-var button_add;
-var button_sub;
-
-var gameMode = 'human-computer';
-var difficulty = 'easy';
-
-function centerCanvas() {
-  var x = (windowWidth - width) / 2;
-  var y = (windowHeight - height) / 3;
-  cnv.position(x, y);
-
-  button.position(x + Csize / 2 - 25, y + Csize + 60);
-  button_add.position(x + Csize / 2 - 120, y + Csize + 60);
-  button_sub.position(x + Csize / 2 + 55, y + Csize + 60);
-
-  var x1 = windowWidth - 100 - 3 * textWidth(s);
-  h1.position(x1, windowHeight - 100);
-
-  update_GSize();
-
-  p1.position(x, y - 60);
-  p2.position(x + Csize - 120, y - 60);
-}
-
-function windowResized() {
-  centerCanvas();
-}
-
-function initialize() {
-  board = new Board(Csize, cells, gameMode, difficulty);
-
-  Players = board.selectPiece();
-
-  grd.html(cells + "x" + cells);
-  update_GSize();
-
-  p1.html("Player 1: " + Players[0]);
-  p2.html("Player 2: " + Players[1]);
-}
 
 function setup() {
-  Csize = 403;
-  cells = 3;
-  LIMIT = 15;
+  createCanvas(403, 453);
 
-  document.body.style.backgroundColor = color(9, 2, 82);
+  var gameMode = document.querySelector('input[name="game-mode"]:checked').value;
+  var difficulty = document.querySelector('input[name="difficulty"]:checked').value;
 
-  cnv = createCanvas(Csize, Csize + 50);
-  cnv.style("display", "block");
+  board = new Board(403, 3, gameMode, difficulty);
+  board.initialize();
 
-  button = createButton("Reset")
+  var resetButton = createButton("Reset")
     .style("border-radius", "8px")
-    .style("background-color", "lime");
-
-  button_add = createButton("Add Cell")
-    .style("border-radius", "50px")
-    .style("background-color", "coral");
-  button_sub = createButton("Sub Cell")
-    .style("border-radius", "50px")
-    .style("background-color", "coral");
-
-  button.mousePressed(reset);
-  button_add.mousePressed(cells_Add);
-  button_sub.mousePressed(cells_Sub);
-
-  h3 = createElement("h3", "This is a Simple Tic-Tac-Toe game made with JavaScript (p5). (Press 'r/R' or 'Reset' button to reset the game) v1.0")
-    .style("color", "White")
-    .style("background-color", "purple");
-  s = "~ by Diptanu Roy";
-  h1 = createElement("h1", s).style("color", "Yellow");
-  grd = createElement("h1", "").style("color", "White");
-
-  p1 = createElement("h2", "").style("color", "Red");
-  p2 = createElement("h2", "").style("color", "Lime");
-
-  initialize();
-
-  centerCanvas();
+    .style("background-color", "lime")
+    .position(164, 420);
+  resetButton.mousePressed(resetGame);
 }
 
 function draw() {
+  background(9, 2, 82);
   board.show();
 }
 
 function mousePressed() {
-  if (gameMode === "human-computer" && board.index === 1) {
-    return; // Ignore human input when it's the computer's turn
-  }
-  board.turn();
-}
-
-function reset() {
-  initialize();
-  loop();
-}
-
-function cells_Add() {
-  if (cells < LIMIT) {
-    cells++;
-    reset();
+  if (!board.isGameOver() && !board.isComputerTurn()) {
+    var x = floor(mouseX / board.cellSize);
+    var y = floor(mouseY / board.cellSize);
+    board.makeMove(x, y);
   }
 }
 
-function cells_Sub() {
-  if (cells > 3) {
-    cells--;
-    reset();
-  }
-}
-
-function keyPressed() {
-  if (key === "r" || key === "R") {
-    reset();
-  }
-}
-
-function update_GSize() {
-  if (cells < 10) {
-    gs = 20.8;
-  } else {
-    gs = 2 * 20.8;
-  }
-
-  grd.position((windowWidth - width) / 2 + Csize / 2 - gs, (windowHeight - height) / 3 - 80);
+function resetGame() {
+  board.reset();
 }
