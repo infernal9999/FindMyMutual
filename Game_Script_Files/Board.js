46c660490a1b7cb09607bc71f5150df7f6a75335
Diptanu Roy <49843997+infernal9999@users.noreply.github.com>
2023-07-10 20:52:07 +0530

    Update Board.js

--- Game_Script_Files/Board.js
+++ Game_Script_Files/Board.js
@@ -2,508 +2,230 @@ function Board(CSize, cells, mode, difficulty) {
   this.cells = cells;
   this.mode = mode;
   this.difficulty = difficulty;
-
-  this.w = round((CSize - 2) / this.cells);
+  this.cellSize = CSize / this.cells;
 
   this.board = [];
   for (var i = 0; i < this.cells; i++) {
     this.board[i] = [];
   }
 
-  for (i = 0; i < this.cells; i++) {
-    for (var j = 0; j < this.cells; j++) {
-      this.board[i][j] = new Cell(j * this.w, i * this.w, this.w);
-    }
-  }
+  this.currentPlayer = "X";
+  this.computerPlayer = "O";
+  this.humanPlayer = "X";
+  this.gameOver = false;
+  this.computerTurn = false;
 
-  this.index;
-  this.Player_1;
-  this.Player_2;
-
-  this.selectPiece = function () {
-    if (this.mode === 'human-computer') {
-      if (random(1) < 0.5) {
-        this.index = 0;
-        this.Player_1 = 0;
-        this.Player_2 = 1;
-
-        return ["X", "O"];
-      } else {
-        this.index = 1;
-        this.Player_1 = 1;
-        this.Player_2 = 0;
-        return ["O", "X"];
-      }
-    } else {
-      this.index = 0;
-      this.Player_1 = 0;
-      this.Player_2 = 1;
-
-      return ["X", "O"];
-    }
-  };
-
-  this.boardShow = function () {
+  this.initialize = function () {
     for (var i = 0; i < this.cells; i++) {
       for (var j = 0; j < this.cells; j++) {
-        stroke(0);
-
-        if (this.Onit(this.board[i][j])) {
-          fill(150, 150, 255, 255);
-        } else {
-          fill(255);
-        }
-
-        rect(this.board[i][j].x, this.board[i][j].y, this.board[i][j].s + 1, this.board[i][j].s + 1);
-
-        if (this.board[i][j].input == "O") {
-          noFill();
-          ellipseMode(CENTER);
-          ellipse(
-            this.board[i][j].cenX,
-            this.board[i][j].cenY,
-            this.board[i][j].s / 1.5
-          );
-        }
-
-        if (this.board[i][j].input == "X") {
-          var offset = this.board[i][j].s / 1.3;
-          line(
-            this.board[i][j].x + offset,
-            this.board[i][j].y + offset,
-            this.board[i][j].x + this.board[i][j].s - offset,
-            this.board[i][j].y + this.board[i][j].s - offset
-          );
-          line(
-            this.board[i][j].x + this.board[i][j].s - offset,
-            this.board[i][j].y + offset,
-            this.board[i][j].x + offset,
-            this.board[i][j].y + this.board[i][j].s - offset
-          );
-        }
+        this.board[i][j] = "";
       }
     }
-  };
 
-  this.Player_update = function () {
-    background(0);
-    textAlign(CENTER, TOP);
-    textSize(24);
-
-    if (this.index === this.Player_1) {
-      fill(255, 0, 0);
-      text("Player 1", width / 2, height - 35);
-    } else {
-      fill(0, 255, 0);
-      text("Player 2", width / 2, height - 35);
+    if (this.mode === "human-computer" && this.currentPlayer === this.computerPlayer) {
+      this.computerTurn = true;
+      this.makeComputerMove();
     }
   };
 
   this.show = function () {
-    var Winner = this.check_Win();
-
-    if (Winner[0] !== 1 && Winner[0] !== 2) {
-      this.Player_update();
-      this.boardShow();
-
-      fill(255, 0, 255);
-      text("Turn: ", width / 2 - textWidth("Player 1"), height - 35);
-    } else {
-      if (Winner[0] === 1) {
-        if (this.index === 0) {
-          this.index = 1;
-        } else {
-          this.index = 0;
-        }
-
-        this.Player_update();
-        this.boardShow();
-        this.drawLine(Winner[1], Winner[2]);
-
-        fill(0, 100, 255);
-        text(
-          "WON",
-          width / 2 + textWidth("Player 1") + 10,
-          height - 35
-        );
-      } else if (Winner[0] === 2) {
-        background(0);
-        this.boardShow();
-
-        fill(0, 100, 255);
-        text("TIE", width / 2, height - 35);
-      }
-
-      noLoop();
-    }
-  };
-
-  this.Onit = function (Cell) {
-    var x1 = Cell.x;
-    var y1 = Cell.y;
-    var x2 = Cell.x + Cell.s;
-    var y2 = Cell.y + Cell.s;
-
-    if (
-      mouseX >= x1 &&
-      mouseX <= x2 &&
-      mouseY >= y1 &&
-      mouseY <= y2
-    ) {
-      return true;
-    }
-  };
-
-  this.turn = function () {
-    if (this.mode === 'human-computer' && this.index === 1) {
-      return; // Ignore human input when it's the computer's turn
-    }
     for (var i = 0; i < this.cells; i++) {
       for (var j = 0; j < this.cells; j++) {
-        if (this.Onit(this.board[i][j])) {
-          if (
-            this.board[i][j].input !== "X" &&
-            this.board[i][j].input !== "O"
-          ) {
-            if (this.index === 1) {
-              this.board[i][j].input = "O";
-            } else {
-              this.board[i][j].input = "X";
-            }
+        var x = j * this.cellSize;
+        var y = i * this.cellSize;
 
-            this.index++;
+        stroke(255);
+        noFill();
+        rect(x, y, this.cellSize, this.cellSize);
 
-            if (this.index > 1) {
-              this.index = 0;
-            }
-
-            return this.index;
-          }
+        var value = this.board[i][j];
+        if (value !== "") {
+          textAlign(CENTER, CENTER);
+          textSize(48);
+          fill(255);
+          text(value, x + this.cellSize / 2, y + this.cellSize / 2);
         }
       }
     }
-    return this.index;
+
+    if (this.gameOver) {
+      textAlign(CENTER, CENTER);
+      textSize(48);
+      fill(0, 100, 255);
+      text("Game Over", width / 2, height - 40);
+    }
   };
 
-  this.drawLine = function (x, y) {
-    var x1;
-    var y1;
-    var x2;
-    var y2;
+  this.makeMove = function (x, y) {
+    if (this.board[y][x] === "" && !this.gameOver) {
+      this.board[y][x] = this.currentPlayer;
+      if (this.checkWin(this.currentPlayer)) {
+        this.gameOver = true;
+      } else if (this.checkTie()) {
+        this.gameOver = true;
+      } else {
+        this.currentPlayer = this.currentPlayer === "X" ? "O" : "X";
 
-    stroke(255, 0, 0);
-
-    // Drawing line
-
-    // Verticals
-    if (y !== -1) {
-      x1 = this.board[y][0].cenX;
-      y1 = this.board[y][0].cenY;
-      x2 = this.board[y][this.cells - 1].cenX;
-      y2 = y1;
-
-      line(x1, y1, x2, y2);
+        if (this.mode === "human-computer" && this.currentPlayer === this.computerPlayer) {
+          this.computerTurn = true;
+          this.makeComputerMove();
+        }
+      }
     }
-
-    // Horizontals
-    if (x !== -1 && x !== "LD" && x !== "RD") {
-      x1 = this.board[0][x].cenX;
-      y1 = this.board[0][x].cenY;
-      x2 = x1;
-      y2 = this.board[this.cells - 1][x].cenY;
-
-      line(x1, y1, x2, y2);
-    }
-
-    // Left Diagonal
-    if (x === "LD") {
-      x1 = this.board[0][0].cenX;
-      y1 = this.board[0][0].cenY;
-      x2 = this.board[this.cells - 1][this.cells - 1].cenX;
-      y2 = this.board[this.cells - 1][this.cells - 1].cenY;
-
-      line(x1, y1, x2, y2);
-    }
-
-    // Right Diagonal
-    if (x === "RD") {
-      x1 = this.board[0][this.cells - 1].cenX;
-      y1 = this.board[0][this.cells - 1].cenY;
-      x2 = this.board[this.cells - 1][0].cenX;
-      y2 = this.board[this.cells - 1][0].cenY;
-
-      line(x1, y1, x2, y2);
-    }
-
-    noStroke();
   };
 
-  this.check_Win = function () {
-    var win = false;
-    var x = -1;
-    var y = -1;
+  this.makeComputerMove = function () {
+    var bestMove = this.findBestMove();
+    var x = bestMove.col;
+    var y = bestMove.row;
+    this.board[y][x] = this.computerPlayer;
+
+    if (this.checkWin(this.computerPlayer)) {
+      this.gameOver = true;
+    } else if (this.checkTie()) {
+      this.gameOver = true;
+    } else {
+      this.currentPlayer = this.currentPlayer === "X" ? "O" : "X";
+      this.computerTurn = false;
+    }
+  };
+
+  this.findBestMove = function () {
+    var bestScore = -Infinity;
+    var bestMove;
 
     for (var i = 0; i < this.cells; i++) {
-      if (win === false) {
-        // Verticals
-        var temp = this.board[i][0].input;
-        if (temp !== undefined) {
-          var flag = 0;
+      for (var j = 0; j < this.cells; j++) {
+        if (this.board[i][j] === "") {
+          this.board[i][j] = this.computerPlayer;
+          var score = this.minimax(this.board, 0, false);
+          this.board[i][j] = "";
 
-          for (var j = 1; j < this.cells; j++) {
-            if (temp === this.board[i][j].input) {
-              temp = this.board[i][j].input;
-            } else {
-              flag = 1;
-              break;
-            }
+          if (score > bestScore) {
+            bestScore = score;
+            bestMove = { row: i, col: j };
           }
-
-          if (flag === 0) {
-            y = i;
-            win = true;
-          }
-        }
-      }
-
-      if (win === false) {
-        // Horizontals
-        temp = this.board[0][i].input;
-        if (temp !== undefined) {
-          flag = 0;
-
-          for (j = 1; j < this.cells; j++) {
-            if (temp === this.board[j][i].input) {
-              temp = this.board[j][i].input;
-            } else {
-              flag = 1;
-              break;
-            }
-          }
-
-          if (flag === 0) {
-            x = i;
-            win = true;
-          }
-        }
-      }
-
-      if (win) break;
-    }
-
-    if (win === false) {
-      // Left Diagonal
-      temp = this.board[0][0].input;
-      if (temp !== undefined) {
-        flag = 0;
-        for (i = 1; i < this.cells; i++) {
-          if (temp === this.board[i][i].input) {
-            temp = this.board[i][i].input;
-          } else {
-            flag = 1;
-            break;
-          }
-        }
-
-        if (flag === 0) {
-          x = "LD";
-          win = true;
         }
       }
     }
 
-    if (win === false) {
-      // Right Diagonal
-      temp = this.board[0][this.cells - 1].input;
-      if (temp !== undefined) {
-        flag = 0;
-        for (i = 1, j = this.cells - 2; i < this.cells; i++, j--) {
-          if (temp === this.board[i][j].input) {
-            temp = this.board[i][j].input;
-          } else {
-            flag = 1;
-            break;
-          }
-        }
-
-        if (flag === 0) {
-          x = "RD";
-          win = true;
-        }
-      }
-    }
-
-    if (win) {
-      return [1, x, y];
-    }
-
-    // Tie check
-    if (win === false) {
-      var count = 0;
-      for (i = 0; i < this.cells; i++) {
-        for (j = 0; j < this.cells; j++) {
-          if (this.board[i][j].input !== undefined) {
-            count++;
-          } else break;
-        }
-      }
-      if (count == this.cells * this.cells) {
-        return [2, x, y];
-      }
-    }
-
-    return [0, x, y];
+    return bestMove;
   };
-}
 
-function Cell(xn, yn, sn) {
-  this.x = xn;
-  this.y = yn;
-  this.s = sn;
-  this.input = undefined;
+  this.minimax = function (board, depth, isMaximizing) {
+    if (this.checkWin(this.computerPlayer)) {
+      return 1;
+    } else if (this.checkWin(this.humanPlayer)) {
+      return -1;
+    } else if (this.checkTie()) {
+      return 0;
+    }
 
-  this.cenX = this.x + this.s / 2;
-  this.cenY = this.y + this.s / 2;
-}
+    if (isMaximizing) {
+      var bestScore = -Infinity;
 
+      for (var i = 0; i < this.cells; i++) {
+        for (var j = 0; j < this.cells; j++) {
+          if (board[i][j] === "") {
+            board[i][j] = this.computerPlayer;
+            var score = this.minimax(board, depth + 1, false);
+            board[i][j] = "";
+            bestScore = max(score, bestScore);
+          }
+        }
+      }
 
-var Csize;
-var cells;
-var LIMIT;
-var board;
-var Players;
-var p1;
-var p2;
+      return bestScore;
+    } else {
+      var bestScore = Infinity;
 
-var cnv;
-var h1;
-var h3;
-var s;
-var grd;
-var gs;
-var button;
-var button_add;
-var button_sub;
+      for (var i = 0; i < this.cells; i++) {
+        for (var j = 0; j < this.cells; j++) {
+          if (board[i][j] === "") {
+            board[i][j] = this.humanPlayer;
+            var score = this.minimax(board, depth + 1, true);
+            board[i][j] = "";
+            bestScore = min(score, bestScore);
+          }
+        }
+      }
 
-var gameMode = 'human-computer';
-var difficulty = 'easy';
+      return bestScore;
+    }
+  };
 
-function centerCanvas() {
-  var x = (windowWidth - width) / 2;
-  var y = (windowHeight - height) / 3;
-  cnv.position(x, y);
+  this.checkWin = function (player) {
+    // Check rows
+    for (var i = 0; i < this.cells; i++) {
+      var rowWin = true;
+      for (var j = 0; j < this.cells; j++) {
+        if (this.board[i][j] !== player) {
+          rowWin = false;
+          break;
+        }
+      }
+      if (rowWin) {
+        return true;
+      }
+    }
 
-  button.position(x + Csize / 2 - 25, y + Csize + 60);
-  button_add.position(x + Csize / 2 - 120, y + Csize + 60);
-  button_sub.position(x + Csize / 2 + 55, y + Csize + 60);
+    // Check columns
+    for (var i = 0; i < this.cells; i++) {
+      var colWin = true;
+      for (var j = 0; j < this.cells; j++) {
+        if (this.board[j][i] !== player) {
+          colWin = false;
+          break;
+        }
+      }
+      if (colWin) {
+        return true;
+      }
+    }
 
-  var x1 = windowWidth - 100 - 3 * textWidth(s);
-  h1.position(x1, windowHeight - 100);
+    // Check diagonals
+    var diagonal1Win = true;
+    var diagonal2Win = true;
+    for (var i = 0; i < this.cells; i++) {
+      if (this.board[i][i] !== player) {
+        diagonal1Win = false;
+      }
+      if (this.board[i][this.cells - 1 - i] !== player) {
+        diagonal2Win = false;
+      }
+    }
 
-  update_GSize();
+    if (diagonal1Win || diagonal2Win) {
+      return true;
+    }
 
-  p1.position(x, y - 60);
-  p2.position(x + Csize - 120, y - 60);
-}
+    return false;
+  };
 
-function windowResized() {
-  centerCanvas();
-}
+  this.checkTie = function () {
+    for (var i = 0; i < this.cells; i++) {
+      for (var j = 0; j < this.cells; j++) {
+        if (this.board[i][j] === "") {
+          return false;
+        }
+      }
+    }
+    return true;
+  };
 
-function initialize() {
-  board = new Board(Csize, cells, gameMode, difficulty);
+  this.isGameOver = function () {
+    return this.gameOver;
+  };
 
-  Players = board.selectPiece();
+  this.isComputerTurn = function () {
+    return this.computerTurn;
+  };
 
-  grd.html(cells + "x" + cells);
-  update_GSize();
-
-  p1.html("Player 1: " + Players[0]);
-  p2.html("Player 2: " + Players[1]);
-}
-
-function setup() {
-  Csize = 403;
-  cells = 3;
-  LIMIT = 15;
-
-  document.body.style.backgroundColor = color(9, 2, 82);
-
-  cnv = createCanvas(Csize, Csize + 50);
-  cnv.style("display", "block");
-
-  button = createButton("Reset")
-    .style("border-radius", "8px")
-    .style("background-color", "lime");
-
-  button_add = createButton("Add Cell")
-    .style("border-radius", "50px")
-    .style("background-color", "coral");
-  button_sub = createButton("Sub Cell")
-    .style("border-radius", "50px")
-    .style("background-color", "coral");
-
-  button.mousePressed(reset);
-  button_add.mousePressed(cells_Add);
-  button_sub.mousePressed(cells_Sub);
-
-  h3 = createElement("h3", "This is a Simple Tic-Tac-Toe game made with JavaScript (p5). (Press 'r/R' or 'Reset' button to reset the game) v1.0")
-    .style("color", "White")
-    .style("background-color", "purple");
-  s = "~ by Diptanu Roy";
-  h1 = createElement("h1", s).style("color", "Yellow");
-  grd = createElement("h1", "").style("color", "White");
-
-  p1 = createElement("h2", "").style("color", "Red");
-  p2 = createElement("h2", "").style("color", "Lime");
-
-  initialize();
-
-  centerCanvas();
-}
-
-function draw() {
-  board.show();
-}
-
-function mousePressed() {
-  if (gameMode === "human-computer" && board.index === 1) {
-    return; // Ignore human input when it's the computer's turn
-  }
-  board.turn();
-}
-
-function reset() {
-  initialize();
-  loop();
-}
-
-function cells_Add() {
-  if (cells < LIMIT) {
-    cells++;
-    reset();
-  }
-}
-
-function cells_Sub() {
-  if (cells > 3) {
-    cells--;
-    reset();
-  }
-}
-
-function keyPressed() {
-  if (key === "r" || key === "R") {
-    reset();
-  }
-}
-
-function update_GSize() {
-  if (cells < 10) {
-    gs = 20.8;
-  } else {
-    gs = 2 * 20.8;
-  }
-
-  grd.position((windowWidth - width) / 2 + Csize / 2 - gs, (windowHeight - height) / 3 - 80);
+  this.reset = function () {
+    this.board = [];
+    this.currentPlayer = "X";
+    this.gameOver = false;
+    this.computerTurn = false;
+    this.initialize();
+  };
 }
